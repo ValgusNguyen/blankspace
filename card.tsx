5ccdbce4abaa611baf428670431118d3168d32cc
Valgus <khuenguyenhaminh@gmail.com>
2025-09-13 17:27:09 +0700

    delete the save note button

--- card.tsx
+++ card.tsx
@@ -1,11 +1,9 @@
 
 export default function Cards(){
-    <div>
-    <figure>
-        <img src="/home/valgus/project/valgus/33b66141-284b-4317-a25f-8c6cfa419ce7.jpeg" width={200} height={300} alt="testing image" />
-    </figure>
-    <div>
-        <h1>Note number 1</h1>
+    <div className= "w-1.5 h-4 bg-black">
+        <div>
+            <span className="w-1 h-4 bg-amber-300">Blank Space</span>
+        </div>
+
     </div>
-</div>
 }
