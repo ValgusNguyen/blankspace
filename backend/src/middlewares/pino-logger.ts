d863d19b6c586514551b7816f19969722067edec
T2Knock <huynhtransongngoc@gmail.com>
2025-11-19 21:00:25 +0700

    chore: simplify logger

--- backend/src/middlewares/pino-logger.ts
+++ backend/src/middlewares/pino-logger.ts
@@ -1,10 +1,28 @@
 import { env } from "@/lib/env";
 import { pinoLogger as logger } from "hono-pino";
-import pino from "pino";
-import pretty from "pino-pretty";
+import pino, { type LoggerOptions } from "pino";
+
+const baseOptions: LoggerOptions = {
+  level: env.NODE_ENV === "production" ? "info" : "debug",
+};
+
+const devConfig: LoggerOptions = {
+  transport: {
+    target: "hono-pino/debug-log",
+  },
+};
+
+const prodConfig: LoggerOptions = {
+  level: "info",
+};
+
+const pinoConfig =
+  env.NODE_ENV === "development"
+    ? { ...baseOptions, ...devConfig }
+    : { ...baseOptions, ...prodConfig };
 
 export function pinoLogger() {
   return logger({
-    pino: pino({}, env.NODE_ENV === "development" ? pretty() : undefined),
+    pino: pino(pinoConfig),
   });
 }
