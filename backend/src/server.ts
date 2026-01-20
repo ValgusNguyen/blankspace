84cab244893b997d64754ec224e6b7c0e0a96ba0
T2Knock <huynhtransongngoc@gmail.com>
2026-01-20 14:34:21 +0700

    chore: update script

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,19 +2,17 @@ import { serve } from "bun";
 import { app } from "./app";
 import { env } from "./lib/env";
 
-const port = env.PORT ?? 3001 ;
-const server = serve(
-  {
-    fetch: app.fetch,
-    port,
-  },
-);
+const port = env.PORT ?? 3001;
+const server = serve({
+  fetch: app.fetch,
+  port,
+});
 
 console.log(`Server is running on http://localhost:${server.port}`);
 
 const shutdown = () => {
   console.log("\nShutting down server blankspace...");
-  server.stop()
+  server.stop();
 
   console.log("Server close successfully.");
   process.exit(0);
